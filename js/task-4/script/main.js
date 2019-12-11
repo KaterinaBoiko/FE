71a11f3cd3ec658f6aa557c837a4e8854b3dc42f
Kateryna Boiko <katerina.boiko16@gmail.com>
2019-12-11 21:28:02 +0200

    index.html in root

---
+++ js/task-4/script/main.js
@@ -0,0 +1,23 @@
+let start = 0;
+let end = 100;
+
+
+
+//var start = parseInt(document.getElementById("start").value);
+//var end = parseInt(document.getElementById("end").value);
+
+if (isNaN(start) || isNaN(end))
+    alert("Incorrect input");
+for (var i = 0; i < n; i++) {
+    //document.getElementById("output").innerHTML += gen.next(fw).value + " | ";
+};
+
+
+
+
+
+
+
+
+
+
