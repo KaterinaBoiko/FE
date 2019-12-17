5f6430d478f470e059c46e39094e72bd3b937d16
Kateryna Boiko <katerina.boiko16@gmail.com>
2019-12-17 23:22:04 +0200

    js/task-3,4

--- js/task-4/script/main.js
+++ js/task-4/script/main.js
@@ -1,17 +1,64 @@
-let start = 0;
-let end = 100;
+let numberToGuess = Math.floor(Math.random() * 101);
+let attemptCount = 0;
+let compAttemptCount = 0;
+let compNumber = document.getElementById("compNumber");
+compNumber.value = Math.floor(Math.random() * 101);
+document.getElementsByClassName("computer_guesing")[0].classList.add("disabled");
 
+function guess() {
+    let attempt = parseInt(document.getElementById("number").value);
+    let start = document.getElementById("start");
+    let end = document.getElementById("end");
+    let hint = document.getElementById("hint");
 
+    if (isNaN(attempt) || attempt < start.value || attempt > end.value)
+        alert("Incorrect input");
+    else {
+        attemptCount++;
+        if (attempt > numberToGuess) {
+            hint.innerHTML = "Your variant is more than the right number.";
+            end.value = attempt;
+        }
+        else if (attempt < numberToGuess) {
+            hint.innerHTML = "Your variant is less than the right number.";
+            start.value = attempt;
+        }
+        else {
+            alert(`Congratulation! You guessed it in ${attemptCount} attempts.`);
+            document.location.reload();
+        }
+        document.getElementsByClassName("user_guessing")[0].classList.add("disabled");
+        document.getElementsByClassName("computer_guesing")[0].classList.remove("disabled");
+    
+    }
+}
 
-//var start = parseInt(document.getElementById("start").value);
-//var end = parseInt(document.getElementById("end").value);
+function less(){
+    compAttemptCount++;
+    compStart.value = compNumber.value;
+    max = parseInt(compEnd.value);
+    min = parseInt(compStart.value);
+    compNumber.value = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementsByClassName("user_guessing")[0].classList.remove("disabled");
+    document.getElementsByClassName("computer_guesing")[0].classList.add("disabled");
 
-if (isNaN(start) || isNaN(end))
-    alert("Incorrect input");
-for (var i = 0; i < n; i++) {
-    //document.getElementById("output").innerHTML += gen.next(fw).value + " | ";
-};
+}
 
+function more(){
+    compAttemptCount++;
+    compEnd.value = compNumber.value;
+    max = parseInt(compEnd.value);
+    min = parseInt(compStart.value);
+    compNumber.value = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementsByClassName("user_guessing")[0].classList.remove("disabled");
+    document.getElementsByClassName("computer_guesing")[0].classList.add("disabled");
+}
+
+function guessedUserNumber(){
+    compAttemptCount++;
+    alert(`Computer guessed it in ${compAttemptCount} attempts.`);
+    document.location.reload();
+}
 
 
 
